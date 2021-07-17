02b8bb61228bcb9cb3d22e06802d9cc08a6c36fc
Jankku <29043938+Jankku@users.noreply.github.com>
2021-07-17 17:28:46 +0300

    Add logs to logfiles

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,6 +2,7 @@ import { createLogger, format, transports } from 'winston';
 
 const Logger = createLogger({
   level: 'info',
+  defaultMeta: { service: 'entertainmentlist' },
   format: format.combine(
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss',
@@ -10,7 +11,17 @@ const Logger = createLogger({
     format.splat(),
     format.json(),
   ),
-  defaultMeta: { service: 'entertainmentlist' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+  exceptionHandlers: [
+    new transports.File({ filename: 'exceptions.log' }),
+  ],
 });
 
 if (process.env.NODE_ENV !== 'production') {
