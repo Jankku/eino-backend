5c6601b98d83c55b802e8304c4172dab557c4977
Jankku <29043938+Jankku@users.noreply.github.com>
2021-07-22 15:42:16 +0300

    Changed routes

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -4,11 +4,23 @@ import * as movies from '../services/movielist';
 
 const router = express.Router();
 
+// Book routes
+router.get('/books/book/:bookId', books.getBook);
+router.post('/books/add', books.addBookToList);
+router.put('/books/edit/:bookId', books.updateBook);
+router.delete('/books/delete/:bookId', books.deleteBook);
+
 router.get('/books/completed', books.getCompletedList);
 router.get('/books/reading', books.getReadingList);
 router.get('/books/on-hold', books.getOnHoldList);
 router.get('/books/dropped', books.getDroppedList);
 router.get('/books/planned', books.getPlannedList);
+
+// Movie routes
+router.get('/movies/movie/:movieId', movies.getMovie);
+router.post('/movies/add', movies.addMovieToList);
+router.put('/movies/edit/:movieId', movies.updateMovie);
+router.delete('/movies/delete/:movieId', movies.deleteMovie);
 
 router.get('/movies/completed', movies.getCompletedList);
 router.get('/movies/watching', movies.getWatchingList);
