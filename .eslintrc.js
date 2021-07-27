665616e3f54db3bf89dcbcd446ba33e71301e73f
Jankku <29043938+Jankku@users.noreply.github.com>
2021-07-27 17:19:17 +0300

    Disabled eslint naming-convention rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
-    root: true,
-    plugins: [
-      "security"
-    ],
-    extends: [
-      "airbnb-typescript/base",
-      "plugin:security/recommended"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: "./tsconfig.eslint.json",
-    },
-    rules: {
-      "linebreak-style": 0,
-      "consistent-return": 0
-    },
-}
+  root: true,
+  plugins: ['security'],
+  extends: ['airbnb-typescript/base', 'plugin:security/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'consistent-return': 0,
+    '@typescript-eslint/naming-convention': ['off'],
+  },
+};
