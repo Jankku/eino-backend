f1d2e823da4d73dc8b80f432b19109ba761d725d
Jankku <29043938+Jankku@users.noreply.github.com>
2021-07-30 11:05:58 +0300

    Started using db transactions

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,12 +3,31 @@ import Logger from '../util/logger';
 
 require('dotenv').config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  idleTimeoutMillis: 30000,
+});
 
 pool.on('connect', () => Logger.info('Connected to pool'));
 pool.on('error', (err: Error) => Logger.error(err.stack));
 
-const query = (q: QueryConfig, params?: any) => pool.query(q, params);
+const query = async (q: QueryConfig) => {
+  const client = await pool.connect();
+  let res;
+  try {
+    await client.query('BEGIN');
+    try {
+      res = await client.query(q);
+      await client.query('COMMIT');
+    } catch (err) {
+      await client.query('ROLLBACK');
+      throw err;
+    }
+  } finally {
+    client.release();
+  }
+  return res;
+};
 
 export {
   query,
