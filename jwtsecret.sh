2ff330fc5cc7b6ce935830b1000f32c8fbaab1a9
Jankku <29043938+Jankku@users.noreply.github.com>
2024-09-01 19:10:31 +0300

    chore: remove redundant secret

--- jwtsecret.sh
+++ jwtsecret.sh
@@ -4,6 +4,5 @@ set -e
 # Replaces the secret placeholders in the .env file with random 32 byte hex strings
 sed -i "s/ACCESS_TOKEN_SECRET=/ACCESS_TOKEN_SECRET=$(openssl rand -hex 32)/g" .env
 sed -i "s/REFRESH_TOKEN_SECRET=/REFRESH_TOKEN_SECRET=$(openssl rand -hex 32)/g" .env
-sed -i "s/EMAIL_VERIFICATION_SECRET=/EMAIL_VERIFICATION_SECRET=$(openssl rand -hex 32)/g" .env
 
 echo "Secrets generated in .env file"
